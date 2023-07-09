c1d3e33a9b1f34484d2cb3549d9268b663b69b40
Vinod Prajapati <vinodprajapatimj@gmail.com>
2023-07-09 09:50:15 +0530

    working progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', 
+  loadChildren: () => import('./share/share.module').then(m => m.ShareModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/share/share-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MoviesDetailsComponent } from './movies-details/movies-details.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'movies-details', component: MoviesDetailsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ShareRoutingModule { }
