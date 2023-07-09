8d1f3b8df7affc5b11d9c0a8a541cd8801e4b9e4
Vinod Prajapati <vinodprajapatimj@gmail.com>
2023-07-09 20:28:44 +0530

    working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import { MoviesDetailsComponent } from './movies-details/movies-details.componen
 import { MoviesComponent } from './movies/movies.component';
 
 const routes: Routes = [
-  { path: '**',component:HomeComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'movies', component: MoviesComponent },
-  { path: 'movies-details', component: MoviesDetailsComponent},
+  { path: 'movies-details', component: MoviesDetailsComponent },
 ];
 
 @NgModule({
