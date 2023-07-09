216c92ea16262815cb089b5e3bc2e7cda238bfe4
VikashMlv <vinodprajapatimj@gmail.com>
2023-07-09 01:45:08 -0700

    work in progress

--- src/app/share/share-routing.module.ts
+++ src/app/share/share-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MoviesComponent } from './movies/movies.component';
 import { MoviesDetailsComponent } from './movies-details/movies-details.component';
+import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'movies', component: MoviesComponent },
   { path: 'movies-details', component: MoviesDetailsComponent},
-  {path:'**',component:MoviesDetailsComponent}
+  {path:'**',component:HomeComponent}
 ];
 
 @NgModule({
